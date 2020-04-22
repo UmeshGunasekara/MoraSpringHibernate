e6b8e2e7802e31f8c9e9bb01041eef907c23896a
Umesh Gunasekara <umesh.gunasekara.n@gmail.com>
2020-04-22 19:38:27 +0800

    MoraWar - Kanchana Spring Hibernate Teach - Updated - SLMORA - 202004221938

--- src/main/java/com/slmora/common/configuration/AppConfig.java
+++ src/main/java/com/slmora/common/configuration/AppConfig.java
@@ -5,10 +5,15 @@
  */
 package com.slmora.common.configuration;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -47,9 +52,31 @@ public class AppConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        return source;
+    }
+
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
+        validator.setValidationMessageSource(messageSource());
+        return validator;
+    }
+
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        //https://stackoverflow.com/questions/15937592/spring-value-is-not-resolving-to-value-from-property-file
+        //For @Value annotation
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
 /**
